1d0ccb58e9c62a128b3d7b9eee9a6b8c5c7b4463
Ashwin A Kumar <ashwinkumar3098@gmail.com>
2021-05-17 11:17:31 +0530

    Initial Framework Setup for Android

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+// ****************** Player 3D Movement ****************** 
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;
+    public float DeathDistance;
+
+    private Vector3 StartPos;
+    private bool endGame = false;
+
+    public Joystick joystick;
+
+    private void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < DeathDistance)
+        {
+            transform.position = StartPos;
+        }
+        if (!endGame)
+        {
+            MovePlayer();
+        }
+
+    }
+
+    void MovePlayer()
+    {
+
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            Vector3 direction = new Vector3(joystick.Horizontal, 0, joystick.Vertical).normalized;
+            transform.Translate(direction * Time.deltaTime * playerSpeed, Space.World);
+            transform.LookAt(direction + transform.position);
+        }
+        
+    }
+
+    public void EndGame()
+    {
+        endGame = true;
+    }
+
+}
